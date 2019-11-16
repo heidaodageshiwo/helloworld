3e0cc78f522b8b40e646fd8614c4c68510294fb8
zhangqiang <heidaodageshiwo@163.com>
2019-11-16 19:23:45 +0800

    test

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("我勒个去，第一个helloworld");
         /*git config --global user.name "zhangqiang"
-        git config --global user.email "heidaodageshiwo+++++++++1133312"*/
+        git config --global user.email "heidaodageshiwo+++++++++1133312 dasf"*/
     }
 }
